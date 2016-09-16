2e70ca0a78731dd729e7ed23c6bd6e8f6682031d
Viktor Hellaeus <hellaeus@gmail.com>
2016-09-16 19:10:22 +0200

    New wallpaper and fixes after kernel update

---
+++ scripts/xinput.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# natural scrolling
+xinput set-prop 'ETPS/2 Elantech Touchpad' 280 -77 -77
+# palm detection
+xinput set-prop 'ETPS/2 Elantech Touchpad' 293 1
+# palm dimensions
+xinput set-prop 'ETPS/2 Elantech Touchpad' 294 3 1
+# tap time
+xinput set-prop 'ETPS/2 Elantech Touchpad' 273 250
+# double tap right click
+xinput set-prop 'ETPS/2 Elantech Touchpad' 287 0 0 0 2 1 3
+
